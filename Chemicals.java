d289ab0a8b939b95a61f08364dcafa218cb29b3b
roopashreeav6 <91691254+roopashreeav6@users.noreply.github.com>
2021-10-07 19:14:19 +0530

    Add files via upload
    methods example

---
+++ Chemicals.java
@@ -0,0 +1,12 @@
+class Chemicals
+{
+public static void rChemicals(String[] chemicals)
+{
+System.out.println("rChemicals method is invoked.....\nList of chemicals are......");
+int size=chemicals.length;
+for(int i=0;i<size;i++)
+{
+System.out.println(chemicals[i]);
+}
+}
+}
